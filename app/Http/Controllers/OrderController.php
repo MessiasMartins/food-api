7a5469ffdae87eaa324bb1bbce15e1c750d2e928
Messias Martins <messiasmartins@outlook.com>
2022-10-24 10:41:53 -0300

    Implementado cadastro de orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,16 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use App\Models\Product;
+use Carbon\Carbon;
+use http\Client\Response;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Http\Resources\Order\OrderResource;
 use App\Http\Resources\Order\OrderResourceCollection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class OrderController extends Controller
 {
+    public function __construct()
+    {
+
+    }
+
     public function index()
     {
         $order = Order::with('itens')->get();
         return new OrderResourceCollection($order);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $client = Client::findOrFail($request->client_id);
+
+//            $produtos = $request->products;
+
+//            $prods = [];
+//            foreach ($produtos as $produto) {
+//                $prod_aux = Product::find($produto->id);
+//                if(!$prod_aux){
+//                    throw new \Exception("Produto {$produto->id} Não existe!");
+//                }
+//                $prods[] = $prod_aux;
+//            }
+
+            $order = new Order();
+            $order->client_id = $client->id;
+            $order->date = Carbon::now();
+            $order->delivery_address = $request->delivery_address;
+            $order->status = Order::STATUS_AGUARDANDO_PAGAMENTO;
+
+            $order->save();
+
+//            foreach ($prods as $prod) {
+//                $order->products()->save($prod);
+//            }
+
+            DB::commit();
+
+            return response()->json(['status' => 'success', 'order' => $order], 201);
+        }catch (\Exception $ex){
+            DB::rollBack();
+            return response()->json(['status' => 'error', 'erro' => $ex->getMessage()], 401);
+        }
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,16 @@ class Order extends Model
 {
     use HasFactory;
 
+    const STATUS_AGUARDANDO_PAGAMENTO = 1;
+    const STATUS_PAGO = 2;
+    const STATUS_PREPARANDO_ENVIO = 3;
+    const STATUS_ENVIADO = 4;
+    const STATUS_ENTREGE = 5;
+
+    protected $fillable =[
+
+    ];
+
     public function client()
     {
         return $this->hasOne(Client::class,'id', 'client_id');
